51ed5d792750a17d5088fb17e0d157cc7920b325
nishanthan78524 <85433962+nishanthan78524@users.noreply.github.com>
2026-09-19 20:19:06 +0100

    second commit

---
+++ products.js
@@ -0,0 +1,13 @@
+// Sample database. Add your real products here later.
+const PRODUCTS=[
+{barcode:"5012345678901",name:"Coca-Cola Original Taste 500ml"},
+{barcode:"5012345678902",name:"Pepsi 500ml"},
+{barcode:"5012345678903",name:"Monster Energy Original 500ml"},
+{barcode:"5012345678904",name:"Walkers Ready Salted Crisps 65g"},
+{barcode:"5012345678905",name:"Cadbury Dairy Milk 110g"},
+{barcode:"5012345678906",name:"Buldak Original 140g"},
+{barcode:"5012345678907",name:"Milka Alpine Milk 100g"},
+{barcode:"5012345678908",name:"Red Bull 250ml"},
+{barcode:"5012345678909",name:"Pringles Original 165g"},
+{barcode:"5012345678910",name:"Evian Water 500ml"}
+];
